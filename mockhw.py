4f8ef07f745b35da2b4cb9b18208fb8f51b71e0b
anpaul098@gmail.com <anpaul098@gmail.com>
2019-05-21 17:31:25 +0200

    create conversion for nibbles is now taking signal obj.

--- mockhw.py
+++ mockhw.py
@@ -1,6 +1,5 @@
 import csv
 from signaldef import *
-from encode_decode import *
 
 
 def csv_iterator(filename):
@@ -18,28 +17,12 @@ def make_data(row, conversion):
     return timestamp, nibbles
 
 
-def convert_nibble(physical):
+def create_conversion(signal):
     def conversion(phy_value):
-        raw_value = physical.phy2raw(phy_value)
-        nibbles = encode(raw_value, physical.bitwidth)
+        raw_value = signal.physical.phy2raw(phy_value)
+        nibbles = signal.encoding.encode(raw_value)
         return nibbles
     return conversion
 
-# Physical(x1=-40, x2=165, y1=1, y2=26241, bitwidth=16)
-
-if __name__ == '__main__':
-    csv_it = csv_iterator('mockhw_data.csv')
-
-    conv_func = convert_nibble(Physical(x1=-40, x2=165, y1=1, y2=26241, bitwidth=16))
-    print(make_data(next(csv_it), conversion=conv_func))
-    print(make_data(next(csv_it), conversion=conv_func))
-    print(make_data(next(csv_it), conversion=conv_func))
-    print(make_data(next(csv_it), conversion=conv_func))
-    print(make_data(next(csv_it), conversion=conv_func))
-    print(next(csv_it)[0])
-
-    print(next(csv_it)[1])
-    print(next(csv_it))
-    print(next(csv_it))
 
 
