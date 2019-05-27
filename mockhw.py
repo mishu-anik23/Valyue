9e88299c578e3eeb061f5883b859cfc8db12f7a4
anik-mishu23 <anpaul098@gmail.com>
2019-05-27 08:28:11 +0200

    Home development:: add funcs to read & clean the tab separated ascii files and write into csv file.

--- mockhw.py
+++ mockhw.py
@@ -34,10 +34,45 @@ def create_conversion(signal):
     return conversion
 
 
+def process_ascii_file(filename):
+    with open(os.path.join(os.getcwd(), filename)) as fd:
+        lines = fd.readlines()
+        header = lines.pop(2).rstrip('\n').split('\t')
+        data = (elm.rstrip('\n').split('\t') for elm in lines[4:])
+        return header, data
+
+
+def csv_row_generator(header, data):
+    coldict = {}
+    for value in data:
+        coldict[header[0]] = value[0]
+        coldict[header[1]] = value[1]
+        yield coldict
+
+
+def csv_writer(filename, header, data):
+    with open(os.path.join(os.getcwd(), filename), 'w', newline='') as fd:
+        writer = csv.DictWriter(fd, fieldnames=header)
+        writer.writeheader()
+        for row in data:
+            rowdict = {}
+            rowdict[header[0]] = row[0].replace(',', '.')
+            rowdict[header[1]] = row[1].replace(',', '.')
+            writer.writerow(rowdict)
+
+
+
+
 if __name__ == '__main__':
     it_obj = csv_iterator('SENT_Trace_MUX_2016-11-14.csv')
-    print(next(it_obj))
-    print(next(it_obj))
+    #print(next(it_obj))
+    #print(next(it_obj))
+    print(process_ascii_file('out_short.ascii'))
+    header, data = process_ascii_file('out_short.ascii')
+    csv_writer('out_short.csv', header, data)
+
+    #print(next(csv_row_generator(header, data)))
+
 
 
 
