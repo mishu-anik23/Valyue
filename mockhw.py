60b7bbbacb226ee1b87a62d8cd1bf6bdc3682c13
anpaul098@gmail.com <anpaul098@gmail.com>
2019-06-11 18:04:48 +0200

    add MockHW class by TDD is under development

--- mockhw.py
+++ mockhw.py
@@ -1,16 +1,13 @@
+import io
 import csv
 from signaldef import *
 
 
 def csv_read_from_file(filepath):
     with open(filepath) as csvfile:
-        for row in get_csv_dict_reader(csvfile):
+        csv_it = csv.DictReader(csvfile, delimiter=';')
+        for row in csv_it:
             yield row
-
-
-def get_csv_dict_reader(csvfile):
-    csv_it = csv.DictReader(csvfile, delimiter=';')
-    return csv_it
 
 
 def discard_rows(source, criterion):
@@ -25,10 +22,20 @@ def discard_columns(source, to_remove):
         yield {k: v for k, v in row.items() if k not in to_remove}
 
 
+def convert2int(source):
+    for row in source:
+        yield {k: int(v) for k, v in row.items() if k != 'Time'}
+
+
 def type_is_512(row):
     if row['Typ'] == '512':
         return True
     return False
+
+
+def extract_nibbles(row):
+    nibbles = ['N0', 'N1', 'N2', 'N3', 'N4', 'N5', 'N6', 'CRC']
+    return [int(row[nibble]) for nibble in nibbles]
 
 
 def make_data(row, conversion):
@@ -46,19 +53,39 @@ def create_conversion(signal):
     return conversion
 
 
+class MockHw:
+    def __init__(self, source):
+        self.source = source
+
+    def get_value_from_row(self):
+        rowdict = next(self.source)
+        rx_bus_id = rowdict['Bus']
+        typ = rowdict['Typ']
+        data = extract_nibbles(rowdict)
+        rx_time = rowdict['Rx Time']
+        sync = rowdict['Sync Time']
+        return rx_bus_id, typ, 0, data, rx_time, sync
+
+
+
+
 if __name__ == '__main__':
     in_file_pth = os.path.join(os.getcwd(), 'data/SENT_Trace_MUX_2016-11-14.csv')
     to_remove = ['Typ', 'Stat/Err', 'Skipped']
     csv_it = csv_read_from_file(in_file_pth)
-    print(next(csv_it))
-    print(next(csv_it))
-    print(next(csv_it))
-    print(next(csv_it))
     rows_d = discard_rows(source=csv_read_from_file(in_file_pth), criterion=type_is_512)
     rows_dc = discard_columns(rows_d, to_remove)
+    print(next(rows_dc))
     # print(next(rows_dc))
-    # print(next(rows_dc))
-
+    file_content = io.StringIO("""\
+Bus;Typ;Rx Time;Sync Time;N0;N1;N2;N3;N4;N5;N6;CRC
+1;2;3;44;0;1;2;3;14;2;4;1
+4;5;6;66;1;11;3;14;2;4;3;0
+""")
+    csv_dct = csv.DictReader(file_content, delimiter=';')
+    hw_obj = MockHw(source=csv_dct)
+    vals = hw_obj.get_value_from_row()
+    print(vals)
 
 
 
--- test_csv_generator.py
+++ test_csv_generator.py
@@ -3,78 +3,107 @@ from mockhw import *
 
 
 def test_get_csv_dict_reader_from_file_obj():
-    csv_in = io.StringIO("""\
+    file_content = io.StringIO("""\
 A;B;C
 1;2;3
 4;5;6
 """)
-    csv_it = get_csv_dict_reader(csv_in)
+    csv_it = csv.DictReader(file_content, delimiter=';')
 
     assert next(csv_it) == {'A': '1', 'B': '2', 'C': '3'}
     assert next(csv_it) == {'A': '4', 'B': '5', 'C': '6'}
 
 
 def test_discard_rows_for_typ_512():
-    csv_in = io.StringIO("""\
+    file_content = io.StringIO("""\
 Typ
 0
 512
 23
 """)
-    csv_it_discarded_row = discard_rows(source=get_csv_dict_reader(csv_in), criterion=type_is_512)
+    csv_it = csv.DictReader(file_content, delimiter=';')
+    csv_it_discarded_row = discard_rows(source=csv_it, criterion=type_is_512)
 
     assert next(csv_it_discarded_row) == {'Typ': '0'}
     assert next(csv_it_discarded_row) == {'Typ': '23'}
 
 
 def test_discard_columns_simple_case():
-        csv_in = io.StringIO("""\
+    file_content = io.StringIO("""\
 A;B
 1;2
 4;5
 """)
-        remove_list = ['B']
-        csv_it_discarded_cols = discard_columns(source=get_csv_dict_reader(csv_in), to_remove=remove_list)
+    remove_list = ['B']
+    csv_it = csv.DictReader(file_content, delimiter=';')
+    csv_it_discarded_cols = discard_columns(source=csv_it, to_remove=remove_list)
 
-        assert next(csv_it_discarded_cols) == {'A': '1'}
-        assert next(csv_it_discarded_cols) == {'A': '4'}
+    assert next(csv_it_discarded_cols) == {'A': '1'}
+    assert next(csv_it_discarded_cols) == {'A': '4'}
 
 
 def test_discard_columns_all():
-    csv_in = io.StringIO("""\
+    """Removing all cols return the empty mapping."""
+    file_content = io.StringIO("""\
 A;B
 1;2
 4;5
 """)
     remove_list = ['A', 'B']
-    csv_it_discarded_cols = discard_columns(source=get_csv_dict_reader(csv_in), to_remove=remove_list)
+    csv_it = csv.DictReader(file_content, delimiter=';')
+    csv_it_discarded_cols = discard_columns(source=csv_it, to_remove=remove_list)
 
     assert next(csv_it_discarded_cols) == {}
     assert next(csv_it_discarded_cols) == {}
 
 
 def test_discard_columns_empty_remove_list():
-    csv_in = io.StringIO("""\
+    """For empty remove list func returns the existing cols and relavent row mapping."""
+    file_content = io.StringIO("""\
 A
 1
 4
 """)
     remove_list = []
-    csv_it_discarded_cols = discard_columns(source=get_csv_dict_reader(csv_in), to_remove=remove_list)
+    csv_it = csv.DictReader(file_content, delimiter=';')
+    csv_it_discarded_cols = discard_columns(source=csv_it, to_remove=remove_list)
 
     assert next(csv_it_discarded_cols) == {'A': '1'}
     assert next(csv_it_discarded_cols) == {'A': '4'}
 
 
 def test_discard_columns_invalid_remove_list():
-    """"""
-    csv_in = io.StringIO("""\
+    """For invalid remove list func also returns the existing cols and relavent row mapping."""
+    file_content = io.StringIO("""\
 A
 1
 4
 """)
     remove_list = ['C']
-    csv_it_discarded_cols = discard_columns(source=get_csv_dict_reader(csv_in), to_remove=remove_list)
+    csv_it = csv.DictReader(file_content, delimiter=';')
+    csv_it_discarded_cols = discard_columns(source=csv_it, to_remove=remove_list)
 
     assert next(csv_it_discarded_cols) == {'A': '1'}
     assert next(csv_it_discarded_cols) == {'A': '4'}
+
+
+def test_extract_nibbles():
+    row_in = {'N0': '8', 'N1': '10', 'N2': '7', 'N3': '5', 'N4': '10', 'N5': '1',
+              'N6': '1', 'CRC': '1', 'Rx Time': '122770661', 'Res': '0',  'Bus': '1',
+              'Sync Time': '31495'}
+
+    assert extract_nibbles(row_in) == [8, 10, 7, 5, 10, 1, 1, 1]
+
+
+def test_convert2int():
+    file_content = io.StringIO("""\
+A;B;C;Time
+1;2;3;0,234
+4;5;6;0,1e-6
+""")
+    csv_it = csv.DictReader(file_content, delimiter=';')
+    row_converted = convert2int(source=csv_it)
+
+    assert next(row_converted) == {'A': 1, 'B': 2, 'C': 3}
+    assert next(row_converted) == {'A': 4, 'B': 5, 'C': 6}
+
