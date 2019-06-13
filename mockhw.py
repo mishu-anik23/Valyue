d073e6f179066900d67118006add7411b9e52765
anik-mishu23 <anpaul098@gmail.com>
2019-06-13 08:45:48 +0200

    scheduling for mockHW is under dev

--- mockhw.py
+++ mockhw.py
@@ -66,12 +66,19 @@ class MockHw:
         sync = rowdict['Sync Time']
         return rx_bus_id, typ, 0, data, rx_time, sync
 
+    def calc_rx_time_diff(self):
+        rx_time1 = next(self.source)['Rx Time']
+        rx_time2 = next(self.source)['Rx Time']
+        return rx_time2 - rx_time1
+
+
+
 
 
 
 if __name__ == '__main__':
     in_file_pth = os.path.join(os.getcwd(), 'data/SENT_Trace_MUX_2016-11-14.csv')
-    to_remove = ['Typ', 'Stat/Err', 'Skipped']
+    to_remove = ['Stat/Err', 'Skipped']
     csv_it = csv_read_from_file(in_file_pth)
     rows_d = discard_rows(source=csv_read_from_file(in_file_pth), criterion=type_is_512)
     rows_dc = discard_columns(rows_d, to_remove)
@@ -83,9 +90,20 @@ Bus;Typ;Rx Time;Sync Time;N0;N1;N2;N3;N4;N5;N6;CRC
 4;5;6;66;1;11;3;14;2;4;3;0
 """)
     csv_dct = csv.DictReader(file_content, delimiter=';')
-    hw_obj = MockHw(source=csv_dct)
+    hw_obj = MockHw(source=convert2int(rows_dc))
     vals = hw_obj.get_value_from_row()
+    vals1 = hw_obj.get_value_from_row()
+    tdiff = hw_obj.calc_rx_time_diff()
+    #print(tdiff)
     print(vals)
+    print(vals1)
+    print(tdiff)
+    print(hw_obj.calc_rx_time_diff())
+    print(hw_obj.calc_rx_time_diff())
+    print(hw_obj.calc_rx_time_diff())
+    print(hw_obj.calc_rx_time_diff())
+    print(hw_obj.calc_rx_time_diff())
+    print(hw_obj.calc_rx_time_diff())
 
 
 
