741fdc9c03a147ceec7fe81a6d99703fc60ded7d
anik-mishu23 <anpaul098@gmail.com>
2019-06-25 08:38:34 +0200

    initial development of data storing and time scaling.

--- test_tx_scheduling.py
+++ test_tx_scheduling.py
@@ -3,10 +3,10 @@ from mockhw import *
 
 file_content = io.StringIO("""\
 Time;Bus;Typ;N0;N1;N2;N3;N4;N5;N6;CRC;Rx Time;Sync Time
-0,01456917;1;0;1;11;7;4;15;4;15;9;2;31496
-0,02996182;1;0;2;12;7;4;15;5;0;12;3;31496
-0,02996182;1;0;3;13;7;4;14;5;1;12;5;31496
-0,02996182;1;0;4;11;7;4;13;5;0;12;6;31496
+0,01456917;1;0;1;11;7;4;15;4;15;9;122770661;31496
+0,02996182;1;0;2;12;7;4;15;5;0;12;122777518;31496
+0,02996182;1;0;3;13;7;4;14;5;1;12;122785429;31496
+0,02996182;1;0;4;11;7;4;13;5;0;12;122792706;31496
 """)
 csv_it = csv.DictReader(file_content, delimiter=';')
 mock_src = convert2int(source=csv_it)
@@ -15,9 +15,12 @@ mockhw = MockHw(source=mock_src)
 scheduler = sched.scheduler(time.time, time.sleep)
 
 
-def test_txsched_previous_data():
-    txsched = TxScheduler(mockhw, scheduler, trace_data)
+def test_scheduled_data_storage():
+    txsched = TxScheduler(mockhw, scheduler, store_data)
     scheduler.enter(0, 1, txsched.transmit_data)
+    scheduler.run()
 
-
-    assert scheduler.run() == []
+    assert txsched.data_container == [[1, 11, 7, 4, 15, 4, 15, 9],
+                                      [2, 12, 7, 4, 15, 5, 0, 12],
+                                      [3, 13, 7, 4, 14, 5, 1, 12],
+                                      [4, 11, 7, 4, 13, 5, 0, 12]]
--- tx_scheduling.py
+++ tx_scheduling.py
@@ -3,32 +3,10 @@ import time
 from mockhw import *
 
 
+def store_data(container, data):
+    container.append(data)
+    return container
 
-#Scheduler: A.act()
-# A: get row from file
-#A: B.act()
-#B: Scheduler.enter()
-#A: C.act()
-
-# def a():
-#     #row = mockhw.get_value_from_row()
-#     currrent_time = time.ctime(time.time())
-#     print(" Time = {t}.".format(t=currrent_time))
-#     event_scheduler()
-#
-# def event_scheduler():
-#     scheduler.enter(2, 1, a)
-#
-# def print_time():
-#     currrent_time = time.ctime(time.time())
-#     print("Time = {t}".format(t=currrent_time))
-
-def trace_data(data, val):
-    #all_data = []
-    #all_data.extend(data)
-    #return all_data
-    data.append(val)
-    return data
 
 def scale_timestamp(time):
     return time * 2.56e-6
@@ -41,33 +19,32 @@ class TxScheduler:
         self.scheduler = scheduler
         self.previous_data = None
         self.previous_ts = scale_timestamp(0)
+
         self.trace = trace
-        self.data = []
+        self.data_container = []
 
     def transmit_data(self):
         try:
             row = self.mockhw.get_value_from_row()
         except StopIteration:
-            #currrent_time = time.ctime(time.time())
-            currrent_time = time.time()
-            # print("on Time = {t} sending Last Data = {d}.".format(t=currrent_time, d=self.previous_data))
-            self.trace(self.data, self.previous_data)
-            print(self.data)
+            self.trace(self.data_container, self.previous_data)
+            print(self.data_container)
             return
 
         if self.previous_data is None:
             # this is the very first time we are called
             self.previous_ts = scale_timestamp(row[4])
-            interval = self.previous_ts
+            start_time = scale_timestamp(row[4]) - self.previous_ts
+            interval = start_time
         else:
-            currrent_time = time.ctime(time.time())
-            #print("on Time = {t} sending Data = {d}.".format(t=currrent_time, d=self.previous_data))
-            self.trace(self.data, self.previous_data)
+            self.trace(self.data_container, self.previous_data)
             interval = scale_timestamp(row[4]) - self.previous_ts
             self.previous_ts = scale_timestamp(row[4])
+
         self.previous_data = row[3]
         self.event_scheduler(interval)
-        print(self.data)
+        print(self.data_container)
+        print(len(self.data_container))
         print(interval)
 
     def event_scheduler(self, interval):
@@ -83,7 +60,15 @@ Time;Bus;Typ;N0;N1;N2;N3;N4;N5;N6;CRC;Rx Time;Sync Time
 0,02996182;1;0;4;11;7;4;13;5;0;12;122792706;31496
 """)
     csv_it = csv.DictReader(file_content, delimiter=';')
-    mock_src = convert2int(source=csv_it)
+    #mock_src = convert2int(source=csv_it)
+
+    in_file_pth = os.path.join(os.getcwd(), 'data/SENT_Trace_MUX_2016-11-14.csv')
+    to_remove = ['Stat/Err', 'Skipped']
+    rows_d = discard_rows(source=csv_read_from_file(in_file_pth), criterion=type_is_512)
+    rows_dc = discard_columns(rows_d, to_remove)
+
+    mock_src = convert2int(source=rows_dc)
+
     mockhw = MockHw(source=mock_src)
 
     scheduler = sched.scheduler(time.time, time.sleep)
@@ -92,7 +77,7 @@ Time;Bus;Typ;N0;N1;N2;N3;N4;N5;N6;CRC;Rx Time;Sync Time
 
     print("Start :", now)
 
-    txsched = TxScheduler(mockhw, scheduler, trace=trace_data)
+    txsched = TxScheduler(mockhw, scheduler, trace=store_data)
 
     scheduler.enter(0, 1, txsched.transmit_data)
     #scheduler.enter(0, 1, txsched.c)
