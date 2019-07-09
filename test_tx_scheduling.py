2903552fb0c79636b6e0409ebeba91bb65d156fa
anpaul098@gmail.com <anpaul098@gmail.com>
2019-07-09 18:41:40 +0200

    reaarange the code, Test cases under dev

--- test_tx_scheduling.py
+++ test_tx_scheduling.py
@@ -1,34 +1,22 @@
-from tx_scheduling import *
+import pytest
+import sched
+import time
 from mockhw import *
+from tx_scheduling import *
 
 
-def test_all_scheduled_data_in_a_container():
-    file_content = io.StringIO("""\
-Time;Bus;Typ;N0;N1;N2;N3;N4;N5;N6;CRC;Rx Time;Sync Time
-0,01456917;1;0;1;11;7;4;15;4;15;9;122770661;31496
-0,02996182;1;0;2;12;7;4;15;5;0;12;122777518;31496
-0,02996182;1;0;3;13;7;4;14;5;1;12;122785429;31496
-0,02996182;1;0;4;11;7;4;13;5;0;12;122792706;31496
-""")
-    csv_it = csv.DictReader(file_content, delimiter=';')
+class DataContainer:
+    def __init__(self):
+        self.container = []
+        self.time_info = []
 
-    mock_src = convert2int(source=csv_it)
-    mockhw = MockHw(source=mock_src)
-
-    scheduler = sched.scheduler(time.time, time.sleep)
-    dc = DataContainer()
-
-    txsched = TxScheduler(mockhw, scheduler, dc.trace_data)
-    scheduler.enter(0, 1, txsched.transmit_data)
-    scheduler.run()
-
-    assert dc.container == [[1, 11, 7, 4, 15, 4, 15, 9],
-                            [2, 12, 7, 4, 15, 5, 0, 12],
-                            [3, 13, 7, 4, 14, 5, 1, 12],
-                            [4, 11, 7, 4, 13, 5, 0, 12]]
+    def trace_data(self, data):
+        self.container.append(data)
+        self.time_info.append(time.perf_counter())
 
 
-def test_store_scheduled_data_in_container():
+@pytest.fixture()
+def mockhw():
     file_content = io.StringIO("""\
 Time;Bus;Typ;N0;N1;N2;N3;N4;N5;N6;CRC;Rx Time;Sync Time
 0,01456917;1;0;1;11;7;4;15;4;15;9;122770661;31496
@@ -41,21 +29,55 @@ Time;Bus;Typ;N0;N1;N2;N3;N4;N5;N6;CRC;Rx Time;Sync Time
 0,06136637;1;0;8;14;7;11;0;12;0;2;122840561;31495
 """)
     csv_it = csv.DictReader(file_content, delimiter=';')
+
     mock_src = convert2int(source=csv_it)
     mockhw = MockHw(source=mock_src)
+    return mockhw
 
-    scheduler = sched.scheduler(time.time, time.sleep)
-    dc = DataContainer()
 
-    txsched = TxScheduler(mockhw, scheduler, dc.trace_data)
-    scheduler.enter(0, 1, txsched.transmit_data)
-    scheduler.run()
+def test_store_scheduled_time(mockhw):
+    sched3 = sched.scheduler(time.time, time.sleep)
+    dc3 = DataContainer()
 
-    assert dc.container[0] == [1, 11, 7, 4, 15, 4, 15, 9]
-    assert dc.container[2] == [3, 13, 7, 4, 14, 5, 1, 12]
+    txsched3 = TxScheduler(mockhw, sched3, dc3.trace_data)
+    sched3.enter(0, 1, txsched3.transmit_data)
+    sched3.run()
 
-    assert dc.container[-1] == [8, 14, 7, 11, 0, 12, 0, 2]
-    assert dc.container[-2] == [7, 2, 8, 0, 4, 2, 3, 3]
+    assert dc3.time_info[2] - dc3.time_info[1] == pytest.approx((122785429 * 2.56e-6) - (122777518 * 2.56e-6), 0.1)
+
+
+def test_all_scheduled_data_in_a_container(mockhw):
+    sched1 = sched.scheduler(time.time, time.sleep)
+    dc1 = DataContainer()
+
+    txsched1 = TxScheduler(mockhw, sched1, dc1.trace_data)
+    sched1.enter(0, 1, txsched1.transmit_data)
+    sched1.run()
+
+    assert dc1.container == [[1, 11, 7, 4, 15, 4, 15, 9],
+                             [2, 12, 7, 4, 15, 5, 0, 12],
+                             [3, 13, 7, 4, 14, 5, 1, 12],
+                             [4, 11, 7, 4, 13, 5, 0, 12],
+                             [5, 14, 7, 11, 0, 12, 0, 2],
+                             [6, 8, 1, 14, 10, 4, 15, 3],
+                             [7, 2, 8, 0, 4, 2, 3, 3],
+                             [8, 14, 7, 11, 0, 12, 0, 2]]
+
+
+def test_store_scheduled_data_in_container(mockhw):
+    sched2 = sched.scheduler(time.time, time.sleep)
+    dc2 = DataContainer()
+
+    txsched2 = TxScheduler(mockhw, sched2, dc2.trace_data)
+    sched2.enter(0, 1, txsched2.transmit_data)
+    sched2.run()
+
+    assert dc2.container[0] == [1, 11, 7, 4, 15, 4, 15, 9]
+    assert dc2.container[2] == [3, 13, 7, 4, 14, 5, 1, 12]
+
+    assert dc2.container[-1] == [8, 14, 7, 11, 0, 12, 0, 2]
+    assert dc2.container[-2] == [7, 2, 8, 0, 4, 2, 3, 3]
+
 
 
 
--- tx_scheduling.py
+++ tx_scheduling.py
@@ -1,8 +1,3 @@
-import sched
-import time
-from mockhw import *
-
-
 def scale_timestamp(time):
     return time * 2.56e-6
 
@@ -13,7 +8,7 @@ class TxScheduler:
         self.scheduler = scheduler
         self.trace = trace
         self.previous_data = None
-        self.previous_ts = scale_timestamp(0)
+        self.previous_ts = 0.0
 
     def transmit_data(self):
         try:
@@ -39,48 +34,41 @@ class TxScheduler:
         self.scheduler.enter(interval, 1, self.transmit_data)
 
 
-class DataContainer:
-    def __init__(self):
-        self.container = []
-
-    def trace_data(self, data):
-        self.container.append(data)
-
-
-if __name__ == '__main__':
-    file_content = io.StringIO("""\
-Time;Bus;Typ;N0;N1;N2;N3;N4;N5;N6;CRC;Rx Time;Sync Time
-0,01456917;1;0;1;11;7;4;15;4;15;9;122770661;31496
-0,02996182;1;0;2;12;7;4;15;5;0;12;122777518;31496
-0,02996182;1;0;3;13;7;4;14;5;1;12;122785429;31496
-0,02996182;1;0;4;11;7;4;13;5;0;12;122792706;31496
-""")
-    csv_it = csv.DictReader(file_content, delimiter=';')
-    mock_src = convert2int(source=csv_it)
-
-    in_file_pth = os.path.join(os.getcwd(), 'data/SENT_Trace_MUX_2016-11-14.csv')
-    to_remove = ['Stat/Err', 'Skipped']
-    rows_d = discard_rows(source=csv_read_from_file(in_file_pth), criterion=type_is_512)
-    rows_dc = discard_columns(rows_d, to_remove)
-
-    #mock_src = convert2int(source=rows_dc)
-
-    mockhw = MockHw(source=mock_src)
-
-    scheduler = sched.scheduler(time.time, time.sleep)
-
-    now = time.ctime(time.time())
-
-    print("Start :", now)
-
-    dc = DataContainer()
-
-    txsched = TxScheduler(mockhw, scheduler, trace=dc.trace_data)
-
-    scheduler.enter(0, 1, txsched.transmit_data)
-
-    scheduler.run()
-    print(dc.container)
+# if __name__ == '__main__':
+#     file_content = io.StringIO("""\
+# Time;Bus;Typ;N0;N1;N2;N3;N4;N5;N6;CRC;Rx Time;Sync Time
+# 0,01456917;1;0;1;11;7;4;15;4;15;9;122770661;31496
+# 0,02996182;1;0;2;12;7;4;15;5;0;12;122777518;31496
+# 0,02996182;1;0;3;13;7;4;14;5;1;12;122785429;31496
+# 0,02996182;1;0;4;11;7;4;13;5;0;12;122792706;31496
+# """)
+#     csv_it = csv.DictReader(file_content, delimiter=';')
+#     mock_src = convert2int(source=csv_it)
+#
+#     in_file_pth = os.path.join(os.getcwd(), 'data/SENT_Trace_MUX_2016-11-14.csv')
+#     to_remove = ['Stat/Err', 'Skipped']
+#     rows_d = discard_rows(source=csv_read_from_file(in_file_pth), criterion=type_is_512)
+#     rows_dc = discard_columns(rows_d, to_remove)
+#
+#     #mock_src = convert2int(source=rows_dc)
+#
+#     mockhw = MockHw(source=mock_src)
+#
+#     scheduler = sched.scheduler(time.time, time.sleep)
+#
+#     now = time.ctime(time.time())
+#
+#     print("Start :", now)
+#
+#     dc = DataContainer()
+#
+#     txsched = TxScheduler(mockhw, scheduler, trace=dc.trace_data)
+#
+#     scheduler.enter(0, 1, txsched.transmit_data)
+#
+#     scheduler.run()
+#     print(dc.container)
+#     print(dc.time_info)
 
 
 
