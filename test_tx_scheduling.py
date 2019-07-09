19604cb93b25a17e46a553e8674b1928d5956f0c
anik-mishu23 <anpaul098@gmail.com>
2019-07-09 06:15:10 +0200

    modify the test cases based on DataContainer class.

--- test_tx_scheduling.py
+++ test_tx_scheduling.py
@@ -2,39 +2,7 @@ from tx_scheduling import *
 from mockhw import *
 
 
-file_content_g = io.StringIO("""\
-Time;Bus;Typ;N0;N1;N2;N3;N4;N5;N6;CRC;Rx Time;Sync Time
-0,01456917;1;0;1;11;7;4;15;4;15;9;122770661;31496
-0,02996182;1;0;2;12;7;4;15;5;0;12;122777518;31496
-0,02996182;1;0;3;13;7;4;14;5;1;12;122785429;31496
-0,02996182;1;0;4;11;7;4;13;5;0;12;122792706;31496
-""")
-csv_it_g = csv.DictReader(file_content_g, delimiter=';')
-
-mock_src_g = convert2int(source=csv_it_g)
-mockhw_g = MockHw(source=mock_src_g)
-
-scheduler_g = sched.scheduler(time.time, time.sleep)
-
-
-def test_dcobj_previous_data_attrs_only_stores_last_data():
-    txsched = TxScheduler(mockhw_g, scheduler_g, tracing_data)
-    scheduler_g.enter(0, 1, txsched.transmit_data)
-    scheduler_g.run()
-
-    assert txsched.storage.previous_data == [4, 11, 7, 4, 13, 5, 0, 12]
-    assert txsched.storage.previous_data == [4, 11, 7, 4, 13, 5, 0, 12]
-
-
-def test_trace_func_returns_none():
-    txsched = TxScheduler(mockhw_g, scheduler_g, tracing_data)
-    scheduler_g.enter(0, 1, txsched.transmit_data)
-    scheduler_g.run()
-
-    assert tracing_data(txsched.storage.previous_data) is None
-
-
-def test_dcobj_previous_ts_attrs_only_stores_last_timing_info():
+def test_all_scheduled_data_in_a_container():
     file_content = io.StringIO("""\
 Time;Bus;Typ;N0;N1;N2;N3;N4;N5;N6;CRC;Rx Time;Sync Time
 0,01456917;1;0;1;11;7;4;15;4;15;9;122770661;31496
@@ -48,69 +16,19 @@ Time;Bus;Typ;N0;N1;N2;N3;N4;N5;N6;CRC;Rx Time;Sync Time
     mockhw = MockHw(source=mock_src)
 
     scheduler = sched.scheduler(time.time, time.sleep)
+    dc = DataContainer()
 
-    txsched = TxScheduler(mockhw, scheduler, tracing_data)
+    txsched = TxScheduler(mockhw, scheduler, dc.trace_data)
     scheduler.enter(0, 1, txsched.transmit_data)
     scheduler.run()
 
-    assert txsched.storage.previous_ts == 122792706 * 2.56e-6
-
-    assert txsched.storage.interval == 122792706 * 2.56e-6 - 122785429 * 2.56e-6
-
-
-
-# def test_all_scheduled_data_in_a_container():
-#     file_content = io.StringIO("""\
-# Time;Bus;Typ;N0;N1;N2;N3;N4;N5;N6;CRC;Rx Time;Sync Time
-# 0,01456917;1;0;1;11;7;4;15;4;15;9;122770661;31496
-# 0,02996182;1;0;2;12;7;4;15;5;0;12;122777518;31496
-# 0,02996182;1;0;3;13;7;4;14;5;1;12;122785429;31496
-# 0,02996182;1;0;4;11;7;4;13;5;0;12;122792706;31496
-# """)
-#     csv_it = csv.DictReader(file_content, delimiter=';')
-#
-#     mock_src = convert2int(source=csv_it)
-#     mockhw = MockHw(source=mock_src)
-#
-#     scheduler = sched.scheduler(time.time, time.sleep)
-#
-#     txsched = TxScheduler(mockhw, scheduler, tracing_data)
-#     scheduler.enter(0, 1, txsched.transmit_data)
-#     scheduler.run()
-#
-#     assert txsched.storage.container == [[1, 11, 7, 4, 15, 4, 15, 9],
-#                                          [2, 12, 7, 4, 15, 5, 0, 12],
-#                                          [3, 13, 7, 4, 14, 5, 1, 12],
-#                                          [4, 11, 7, 4, 13, 5, 0, 12]]
+    assert dc.container == [[1, 11, 7, 4, 15, 4, 15, 9],
+                            [2, 12, 7, 4, 15, 5, 0, 12],
+                            [3, 13, 7, 4, 14, 5, 1, 12],
+                            [4, 11, 7, 4, 13, 5, 0, 12]]
 
 
 def test_store_scheduled_data_in_container():
-    file_content = io.StringIO("""\
-Time;Bus;Typ;N0;N1;N2;N3;N4;N5;N6;CRC;Rx Time;Sync Time
-0,01456917;1;0;1;11;7;4;15;4;15;9;122770661;31496
-0,02996182;1;0;2;12;7;4;15;5;0;12;122777518;31496
-0,02996182;1;0;3;13;7;4;14;5;1;12;122785429;31496
-0,02996182;1;0;4;11;7;4;13;5;0;12;122792706;31496
-""")
-    csv_it = csv.DictReader(file_content, delimiter=';')
-    mock_src = convert2int(source=csv_it)
-    mockhw = MockHw(source=mock_src)
-
-    scheduler1 = sched.scheduler(time.time, time.sleep)
-
-    txsched1 = TxScheduler(mockhw, scheduler1, tracing_data)
-    scheduler1.enter(0, 1, txsched1.transmit_data)
-    scheduler1.run()
-
-    assert txsched1.storage.container[0][0] == [1, 11, 7, 4, 15, 4, 15, 9]
-    assert txsched1.storage.container[1][0] == [2, 12, 7, 4, 15, 5, 0, 12]
-    assert txsched1.storage.container[2][0] == [3, 13, 7, 4, 14, 5, 1, 12]
-
-    assert txsched1.storage.container[-1][0] == [4, 11, 7, 4, 13, 5, 0, 12]
-    assert txsched1.storage.container[-2][0] == [3, 13, 7, 4, 14, 5, 1, 12]
-
-
-def test_store_scheduled_data_with_sending_time_in_container():
     file_content = io.StringIO("""\
 Time;Bus;Typ;N0;N1;N2;N3;N4;N5;N6;CRC;Rx Time;Sync Time
 0,01456917;1;0;1;11;7;4;15;4;15;9;122770661;31496
@@ -126,17 +44,18 @@ Time;Bus;Typ;N0;N1;N2;N3;N4;N5;N6;CRC;Rx Time;Sync Time
     mock_src = convert2int(source=csv_it)
     mockhw = MockHw(source=mock_src)
 
-    scheduler1 = sched.scheduler(time.time, time.sleep)
+    scheduler = sched.scheduler(time.time, time.sleep)
+    dc = DataContainer()
 
-    txsched1 = TxScheduler(mockhw, scheduler1, tracing_data)
-    scheduler1.enter(0, 1, txsched1.transmit_data)
-    scheduler1.run()
+    txsched = TxScheduler(mockhw, scheduler, dc.trace_data)
+    scheduler.enter(0, 1, txsched.transmit_data)
+    scheduler.run()
 
-    assert txsched1.storage.container[0] == ([1, 11, 7, 4, 15, 4, 15, 9], 122770661*2.56e-6 - 122770661*2.56e-6)
-    assert txsched1.storage.container[2] == ([3, 13, 7, 4, 14, 5, 1, 12], 122785429*2.56e-6 - 122777518*2.56e-6)
+    assert dc.container[0] == [1, 11, 7, 4, 15, 4, 15, 9]
+    assert dc.container[2] == [3, 13, 7, 4, 14, 5, 1, 12]
 
-    assert txsched1.storage.container[-1] == ([8, 14, 7, 11, 0, 12, 0, 2], 122840561*2.56e-6 - 122820800*2.56e-6)
-    assert txsched1.storage.container[-2] == ([7, 2, 8, 0, 4, 2, 3, 3], 122820800*2.56e-6 - 122807016*2.56e-6)
+    assert dc.container[-1] == [8, 14, 7, 11, 0, 12, 0, 2]
+    assert dc.container[-2] == [7, 2, 8, 0, 4, 2, 3, 3]
 
 
 
--- tx_scheduling.py
+++ tx_scheduling.py
@@ -3,70 +3,6 @@ import time
 from mockhw import *
 
 
-# #container = []
-#
-# def tracing_data(data):
-#     #data_container.fill_container()
-#     print(time.time())
-#     return
-#
-#
-# def scale_timestamp(time):
-#     return time * 2.56e-6
-#
-#
-# class TxScheduler:
-#     def __init__(self, mockhw, scheduler, trace):
-#         self.mockhw = mockhw
-#         self.scheduler = scheduler
-#         self.trace = trace
-#         #self.previous_data = None
-#         #self.previous_ts = scale_timestamp(0)
-#         self.storage = DataContainer(data=None)
-#
-#     def transmit_data(self):
-#         try:
-#             row = self.mockhw.get_value_from_row()
-#         except StopIteration:
-#             self.trace(self.storage.previous_data)
-#             #self.trace(self.storage)
-#             return
-#
-#         if self.storage.previous_data is None:
-#             # this is the very first time we are called
-#             self.storage.previous_ts = scale_timestamp(row[4])
-#             start_time = scale_timestamp(row[4]) - self.storage.previous_ts
-#             self.storage.interval = start_time
-#         else:
-#             self.trace(self.storage.previous_data)
-#             #self.trace(self.storage)
-#             self.storage.interval = scale_timestamp(row[4]) - self.storage.previous_ts
-#             self.storage.previous_ts = scale_timestamp(row[4])
-#
-#         self.storage.previous_data = row[3]
-#         self.storage.fill_container()
-#         self.event_scheduler(self.storage.interval)
-#         print(self.storage.previous_ts)
-#
-#         print(self.storage.interval)
-#         print(self.storage.previous_data)
-#         print(self.storage.container)
-#
-#     def event_scheduler(self, interval):
-#         self.scheduler.enter(interval, 1, self.transmit_data)
-#
-#
-# class DataContainer:
-#     def __init__(self, data):
-#         self.previous_data = data
-#         self.container = []
-#         self.previous_ts = scale_timestamp(0)
-#         self.interval = scale_timestamp(0)
-#
-#     def fill_container(self):
-#         self.container.append((self.previous_data, self.interval))
-#         #self.container.append(self.previous_data)
-
 def scale_timestamp(time):
     return time * 2.56e-6
 
@@ -78,7 +14,6 @@ class TxScheduler:
         self.trace = trace
         self.previous_data = None
         self.previous_ts = scale_timestamp(0)
-
 
     def transmit_data(self):
         try:
@@ -99,7 +34,6 @@ class TxScheduler:
 
         self.previous_data = row[3]
         self.event_scheduler(interval)
-        print(interval)
 
     def event_scheduler(self, interval):
         self.scheduler.enter(interval, 1, self.transmit_data)
@@ -111,6 +45,7 @@ class DataContainer:
 
     def trace_data(self, data):
         self.container.append(data)
+
 
 if __name__ == '__main__':
     file_content = io.StringIO("""\
@@ -138,11 +73,14 @@ Time;Bus;Typ;N0;N1;N2;N3;N4;N5;N6;CRC;Rx Time;Sync Time
 
     print("Start :", now)
 
-    txsched = TxScheduler(mockhw, scheduler, trace=tracing_data)
+    dc = DataContainer()
+
+    txsched = TxScheduler(mockhw, scheduler, trace=dc.trace_data)
 
     scheduler.enter(0, 1, txsched.transmit_data)
 
     scheduler.run()
+    print(dc.container)
 
 
 
