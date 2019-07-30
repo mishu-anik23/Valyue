cc3484e612a865220de64e186fb60ac5d659fb0b
anpaul098@gmail.com <anpaul098@gmail.com>
2019-07-30 18:15:57 +0200

    exercise is under dev

--- exercise_sequence.py
+++ exercise_sequence.py
@@ -1,8 +1,9 @@
 #result[j-2] = input[j] + input[j-1] if (input[j-2]==0)
 #result[j-2] = input[j-2]*( input[j] + input[j-1])
 
-def generate_sequence(source):
-    list = []
+
+def mult_add_sequences(source):
+    out_list = []
     j_1 = None
     j_2 = None
     for idx, elm in enumerate(source):
@@ -13,16 +14,17 @@ def generate_sequence(source):
 
         if j_1:
             if source[j_2] == 0:
-                list.append(source[j] + source[j_1])
+                out_list.append(source[j] + source[j_1])
             else:
-                list.append(source[j_2] * (source[j] + source[j_1]))
-    print(list)
-    return list
+                out_list.append(source[j_2] * (source[j] + source[j_1]))
+    return out_list
 
 
 def consecutive_diffs(in_list):
     out_list = []
     prev_idx = None
+    if len(in_list) <= 1:
+        raise ValueError("At least need two elements to calculate consecutive diffs.")
     for idx, elm in enumerate(in_list):
         next_idx = idx
         if idx > 0:
@@ -30,9 +32,6 @@ def consecutive_diffs(in_list):
 
         if prev_idx is not None:
             out_list.append(in_list[next_idx] - in_list[prev_idx])
-        #else:
-         #   out_list.append(in_list[next_idx] - in_list[0])
-    print(out_list)
     return out_list
 
 
@@ -51,20 +50,66 @@ def consecutive_sums(in_list):
 def generate_consecutive_diff(source):
     previous_number = None
     for number in source:
-        if previous_number is None:
-            previous_number = number
-
-        diff = number - previous_number
+        if previous_number is not None:
+            diff = number - previous_number
+            yield diff
         previous_number = number
-        yield diff
 
 
-def generate_consecutive_sum(number_list):
+def generate_cumulative_sum_1(number_list):
     previous_number = 0
     for number in number_list:
         sum = previous_number + number
         previous_number = sum
         yield sum
+
+
+def generate_cumulative_sum(number_list):
+    previous_num = None
+    for number in number_list:
+        if previous_num is None:
+            previous_num = number
+            sum = previous_num
+        else:
+            sum = previous_num + number
+        previous_num = sum
+        yield sum
+
+
+def cumulative_sums(source):
+    out_list = []
+    previous_num = None
+    for elm in source:
+        if previous_num is None:
+            previous_num = elm
+            sum = previous_num
+        else:
+            sum = previous_num + elm
+        out_list.append(sum)
+        previous_num = sum
+    return out_list
+
+
+def current_largest_sequences(source):
+    out_list = []
+    previous_num = None
+    for elm in source:
+        if previous_num is None:
+            previous_num = elm
+        largest = max(elm, previous_num)
+        out_list.append((previous_num, largest))
+        previous_num = elm
+    return out_list
+
+
+def generate_current_and_largest_sequences(source):
+    previous_num = None
+    for elm in source:
+        if previous_num is None:
+            previous_num = elm
+        largest = max(elm, previous_num)
+        yield (previous_num, largest)
+        previous_num = elm
 
 
 def consecutive_sum_pairs(number_list):
--- test_exercise_sequence.py
+++ test_exercise_sequence.py
@@ -1,33 +1,87 @@
+import pytest
 from exercise_sequence import *
 
-def test_sequence_generator_1():
-    input = [1, 2, 3, 0, 5, 6]
-    assert generate_sequence(input) == [1*(2+3), 2*(3+0), 3*(0+5), (5+6)]
+
+def test_mult_add_sequences_1():
+    list_in = [1, 2, 3, 0, 5, 6]
+    assert mult_add_sequences(list_in) == [1 * (2 + 3), 2 * (3 + 0), 3 * (0 + 5), (5 + 6)]
 
 
-def test_sequence_generator_2():
-    input = [1, 2, 3, 5, 8, 13, 21]
-    assert generate_sequence(input) == [1*(2+3), 2*(3+5), 3*(5+8), 5*(8+13), 8*(13+21)]
+def test_mult_add_sequences_2():
+    list_in = [1, 2, 3, 5, 8, 13, 21]
+    assert mult_add_sequences(list_in) == [1 * (2 + 3), 2 * (3 + 5), 3 * (5 + 8), 5 * (8 + 13), 8 * (13 + 21)]
 
 
-def test_sequence_generator_3():
-    input = [-2, -1, 0, -1, 0, 1, 0]
-    assert generate_sequence(input) == [-2*(-1+0), -1*(0-1), (-1+0), -1*(0+1), (1+0)]
+def test_mult_add_sequences_3():
+    list_in = [-2, -1, 0, -1, 0, 1, 0]
+    assert mult_add_sequences(list_in) == [-2 * (-1 + 0), -1 * (0 - 1), (-1 + 0), -1 * (0 + 1), (1 + 0)]
 
 
-def test_consecutive_diffs_1():
-    input = [1, 1, 2, 3, 5, 8, 13]
-    assert consecutive_diffs(input) == [0, 1, 1, 2, 3, 5]
+def test_get_consecutive_diffs_1():
+    list_in = [1, 1, 2, 3, 5, 8, 13]
+    assert consecutive_diffs(list_in) == [0, 1, 1, 2, 3, 5]
 
 
-def test_consecutive_diffs_2():
-    input = [1, 2, 4, 8, 16, 32, 64, 128, 256]
-    assert consecutive_diffs(input) == [1, 2, 4, 8, 16, 32, 64, 128]
+def test_get_consecutive_diffs_2():
+    list_in = [1, 2, 4, 8, 16, 32, 64, 128, 256]
+    assert consecutive_diffs(list_in) == [1, 2, 4, 8, 16, 32, 64, 128]
 
 
-def test_consecutive_diffs_3():
-    input = [-1, 2, -4, 8, -16, 32, -64, 128]
-    assert consecutive_diffs(input) == [3, -6, 12, -24, 48, -96, 192]
+def test_get_consecutive_diffs_3():
+    list_in = [-1, 2, -4, 8, -16, 32, -64, 128]
+    assert consecutive_diffs(list_in) == [3, -6, 12, -24, 48, -96, 192]
+
+
+def test_get_consecutive_diffs_with_empty_list():
+    list_in = []
+    with pytest.raises(ValueError) as exc:
+        consecutive_diffs(list_in)
+        assert 'need two elements' in str(exc)
+
+
+def test_get_consecutive_diffs_with_one_element():
+    list_in = [1]
+    with pytest.raises(ValueError) as exc:
+        consecutive_diffs(list_in)
+        assert 'need two elements' in str(exc)
+
+
+def test_get_consecutive_diffs_with_two_element():
+    list_in = [1, 2]
+    assert consecutive_diffs(list_in) == [1]
+
+
+def test_generate_consecutive_diffs_1():
+    list_in = [1, 1, 2, 3, 5, 8, 13]
+    diffs_generator = generate_consecutive_diff(list_in)
+    assert list(diffs_generator) == [0, 1, 1, 2, 3, 5]
+
+
+def test_generate_consecutive_diffs_2():
+    list_in = [1, 2, 4, 8, 16, 32, 64, 128, 256]
+    diffs_generator = generate_consecutive_diff(list_in)
+    assert list(diffs_generator) == [1, 2, 4, 8, 16, 32, 64, 128]
+
+
+def test_generate_consecutive_diffs_3():
+    list_in = [-1, 2, -4, 8, -16, 32, -64, 128]
+    diffs_generator = generate_consecutive_diff(list_in)
+    assert list(diffs_generator) == [3, -6, 12, -24, 48, -96, 192]
+
+
+def test_generate_consecutive_diffs_with_empty_list():
+    list_in = []
+    diffs_generator = generate_consecutive_diff(list_in)
+    assert list(diffs_generator) == []
+
+
+
+def test_generate_consecutive_diffs_with_one_element():
+    list_in = [1]
+    diffs_generator = generate_consecutive_diff(list_in)
+    with pytest.raises(ValueError) as exc:
+        list(diffs_generator)
+        assert 'need two elements' in str(exc)
 
 
 
@@ -43,18 +97,16 @@ def test_consecutive_sums_2():
 
 def test_consecutive_sums_3():
     input = [-1, 2, -4, 8, -16, 32, -64, 128]
-    assert consecutive_sums(input) == [3, -6, 12, -24, 48, -96, 192]
+    assert consecutive_sums(input) == [1, -2, 4, -8, 16, -32, 64]
 
 
-
-def test_generate_consecutive_diff_between_numbers():
-    generate_diffs = generate_consecutive_diff([1, 1, 2, 3, 5, 8, 13])
-
-    assert list(generate_diffs) == [0, 0, 1, 1, 2, 3, 5]
+def test_cumulative_sums_1():
+    list_in = [1, 2, 3, 4]
+    assert cumulative_sums(list_in) == [1, 3, 6, 10]
 
 
-def test_generate_consecutive_sum_between_numbers():
-    generate_sum = generate_consecutive_sum([1,2,3,4])
+def test_generate_cumulative_sums_1():
+    generate_sum = generate_cumulative_sum([1, 2, 3, 4])
 
     assert list(generate_sum) == [1, 3, 6, 10]
 
@@ -62,4 +114,51 @@ def test_generate_consecutive_sum_between_numbers():
 def test_generate_sums_of_consecutive_pairs():
      generate_sum_pair = consecutive_sum_pairs([1, 2, 3, 4, 5, 6])
 
-     assert list(generate_sum_pair) ==  [(1+2), (3+4), (5+6)]
+     assert list(generate_sum_pair) == [(1+2), (3+4), (5+6)]
+
+
+def test_current_and_largest_sequences_1():
+    list_in = [1, 1, 2, 3, 5]
+    print(current_largest_sequences(list_in))
+    assert current_largest_sequences(list_in) == [(1, 1), (1, 1), (1, 2), (2, 3), (3, 5)]
+
+
+def test_current_and_largest_sequences_2():
+    list_in = [-1, 2, -4, 8, -16, 32]
+    print(current_largest_sequences(list_in))
+    assert current_largest_sequences(list_in) == [(-1, -1), (-1, 2), (2, 2), (-4, 8), (8, 8), (-16, 32)]
+
+
+def test_current_and_largest_sequences_with_empty_list():
+    list_in = []
+    assert current_largest_sequences(list_in) == []
+
+
+def test_current_and_largest_sequences_with_one_element():
+    list_in = [1]
+    assert current_largest_sequences(list_in) == [(1, 1)]
+
+
+def test_generate_current_and_largest_sequences_1():
+    list_in = [1, 1, 2, 3, 5]
+    generator = generate_current_and_largest_sequences(list_in)
+    assert list(generator) == [(1, 1), (1, 1), (1, 2), (2, 3), (3, 5)]
+
+
+def test_generate_current_and_largest_sequences_2():
+    list_in = [-1, 2, -4, 8]
+    generator = generate_current_and_largest_sequences(list_in)
+    assert list(generator) == [(-1, -1), (-1, 2), (2, 2), (-4, 8)]
+
+
+def test_generate_current_and_largest_sequences_with_empty_list():
+    list_in = []
+    generator = generate_current_and_largest_sequences(list_in)
+    assert list(generator) == []
+
+
+def test_generate_current_and_largest_sequences_with_one_element():
+    list_in = [1]
+    generator = generate_current_and_largest_sequences(list_in)
+    assert list(generator) == [(1, 1)]
+
